62fd96afcf468e839fc050d207c63c352080f5d1
Brinda <saibrindanvn@gmail.com>
2026-01-25 21:22:17 +0530

    Update lab3_a9.py
    Added a function to predict class labels using KNN model.

--- lab3_a9.py
+++ lab3_a9.py
@@ -1,1 +1,11 @@
-print("a")
+def get_knn_predictions(knn_model, X_test):
+    # The function predicts the class labels for the test feature vectors
+    predicted_labels = knn_model.predict(X_test)
+    return predicted_labels
+#  main program
+
+# Predicting the class labels for the test data
+y_predicted_test = get_knn_predictions(knn_model, X_test)
+
+print("A9: Predicted class labels for test dataset:")
+print(y_predicted_test)
