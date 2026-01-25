c7b476a3eb712733a04c0d73d3e04044876139b4
Brinda <saibrindanvn@gmail.com>
2026-01-25 21:14:46 +0530

    Implement kNN accuracy calculation and output
    Added a function to calculate kNN accuracy and print the result.

--- lab3_a8.py
+++ lab3_a8.py
@@ -1,1 +1,10 @@
-print("a")
+def calculate_knn_accuracy(knn_model, X_test, y_test):
+    # the function calculates the accuracy of the  kNN classifier(trained)
+    accuracy_value = knn_model.score(X_test, y_test)
+    return accuracy_value
+# mainprogram
+
+# calculating the accuracy of the kNN classifier on test data
+knn_accuracy = calculate_knn_accuracy(knn_model, X_test, y_test)
+
+print("A8: Accuracy of kNN classifier (k = 3):", knn_accuracy)
