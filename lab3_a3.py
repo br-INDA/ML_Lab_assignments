3433a2e6144c38e055f9dc1fee9eccd9b31ceee6
Brinda <saibrindanvn@gmail.com>
2026-01-25 21:10:30 +0530

    Implement feature statistics calculation and plotting
    Added functionality to calculate and display mean and variance of a feature vector, along with plotting its histogram.

--- lab3_a3.py
+++ lab3_a3.py
@@ -1,1 +1,26 @@
-print("a")
+import matplotlib.pyplot as plt
+
+#Calculates mean and variance of a feature vector
+def calculate_feature_statistics(feature_vector):
+    
+
+    mean_value = np.mean(feature_vector)
+    variance_value = np.var(feature_vector)
+    return mean_value, variance_value
+
+#main program
+# takes the first feature
+feature_index = 0
+selected_feature = X_features[:, feature_index]
+
+mean_feature, variance_feature = calculate_feature_statistics(selected_feature)
+
+plt.hist(selected_feature, bins=20)
+plt.xlabel("Feature Value")
+plt.ylabel("Frequency")
+plt.title("A3: Histogram of Feature 0")
+plt.show()
+
+print("A3: Mean of feature:", mean_feature)
+print("A3: Variance of feature:", variance_feature)
+
